629518cb2182062d6c9d4edb38532c29b26e5ab6
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-01 16:00:39 -0600

    ec2 build using shell script with aws cli

--- instance_build.sh
+++ instance_build.sh
@@ -3,7 +3,7 @@
 #Requirements to build Instance
 SG_ID="sg-0e3e5d0160ba94b0b"
 AMI_ID="ami-0220d79f3f480ecf5"
-OUTE53_ZONE_ID="Z032594897Q2I0KZ7GR1"
+ROUTE53_ZONE_ID="Z032594897Q2I0KZ7GR1"
 DOMAIN_NAME="rkak.online"
 
 for instance in $@
@@ -15,50 +15,57 @@ do
             --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
             --query 'Instances[0].InstanceId' \
             --output text )
-    echo "Launched instance with ID: $INSTANCE_ID"
+        echo "Launched instance with ID: $INSTANCE_ID"
     
-    # Wait until the instance is running
-    aws ec2 wait instance-running --instance-ids $INSTANCE_ID
-    echo "Instance $INSTANCE_ID is now running."
+        # Wait until the instance is running
+        aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+        echo "Instance $INSTANCE_ID is now running."
 
-    if [ $instance == "frontend" ]; then
-    #Allocting IP Address
-        IP_ADDRESS=$(
-            aws ec2 describe-instances \
+        if [ $instance == "frontend" ]; then
+        #Allocting IP Address
+            IP_ADDRESS=$(
+                aws ec2 describe-instances \
                 --instance-ids $INSTANCE_ID \
                 --query 'Reservations[0].Instances[0].PublicIpAddress' \
                 --output text
+            )
+            echo "Instance created with Instance ID: $INSTANCE_ID and Public IP: $IP_ADDRESS"
+            RECORD_NAME="$instance.$DOMAIN_NAME"
+        else
+            IP_ADDRESS=$(
+                aws ec2 describe-instances \
+                    --instance-ids $INSTANCE_ID \
+                    --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+                    --output text
+            )
+            echo "Instance created with Instance ID: $INSTANCE_ID and Private IP: $IP_ADDRESS"
+            RECORD_NAME="$instance.$DOMAIN_NAME"
+        fi   
+        # Create a DNS record in Route 53
+        DNS_RECORD=$(
+            aws route53 change-resource-record-sets \
+            --hosted-zone-id $ROUTE53_ZONE_ID \
+            --change-batch '
+            {
+            "Comment": "DNS Record for '$instance'",
+            "Changes": [
+                {
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                    {
+                    "Value": "'$IP_ADDRESS'"
+                    }
+                    ]
+            }
+            }
+        ]
+        }'
         )
-        echo "Instance created with Instance ID: $INSTANCE_ID and Public IP: $IP_ADDRESS"
-        RECORD_NAME="$instance.$DOMAIN_NAME"
-    else
-        IP_ADDRESS=$(
-            aws ec2 describe-instances \
-                --instance-ids $INSTANCE_ID \
-                --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-                --output text
-        )
-        echo "Instance created with Instance ID: $INSTANCE_ID and Private IP: $IP_ADDRESS"
-        RRECORD_NAME="$instance.$DOMAIN_NAME"
-    fi   
-    # Create a DNS record in Route 53
-    DNS_RECORD=$(
-        aws route53 change-resource-record-sets \
-        --hosted-zone-id $OUTE53_ZONE_ID \
-        --change-batch '{
-        "Comment": "DNS Record for '$instance'",
-        "Changes": [{
-        "Action": "CREATE",
-        "ResourceRecordSet": {
-            "Name": "'$RECORD_NAME'",
-            "Type": "A",
-            "TTL": 1,
-            "ResourceRecords": [{"Value": "'$IP_ADDRESS'"}]
-        }
-        }]
-    }'
-    )
-    echo "Created DNS record for $instance.$DOMAIN_NAME pointing to $IP_ADDRESS"
+        echo "Created DNS record for $instance.$DOMAIN_NAME pointing to $IP_ADDRESS"
     done
 
 
