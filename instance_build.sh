70d5149c836577f5f34696590abf85a45e9a5ed8
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-01 16:04:33 -0600

    ec2 build using shell script with aws cli

--- instance_build.sh
+++ instance_build.sh
@@ -4,7 +4,7 @@
 SG_ID="sg-0e3e5d0160ba94b0b"
 AMI_ID="ami-0220d79f3f480ecf5"
 ROUTE53_ZONE_ID="Z032594897Q2I0KZ7GR1"
-DOMAIN_NAME="rkak.online"
+DOMAIN_NAME="rkak87.online"
 
 for instance in $@
 do
