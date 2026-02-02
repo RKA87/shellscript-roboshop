cd3c2c636834ab2bcea4fe982bfe29ea2d52ae2d
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-02 13:57:29 -0600

    mongodb installation using shell script

---
+++ mongodb-v2.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+first we need to check the sudo account access 
+second need log directory
+third lod file creation
+
+#Color Coding
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+RESET="\e[0m"
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+    echo -n "you should be a root user account priveligies to run this script"
+    exit 1
+fi
+
+LOG_DIR=/var/log/shellscript-roboshop
+if [ ! -d $LOG_DIR ]; then
+        echo -n "Creating log directory"
+        mkdir -p $LOG_DIR
+    else:
+        echo -n "log directory already exists"
+fi
+
+LOG_FILE="$LOG_DIR/mongodb.log"
+
+#Status Check Function to validate the installation status
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $RED FAILED $RESET" | tee -a $LOG_FILE #tee command is nothing but to display output on screen and adding into log file
+        exit 1
+    else
+        echo -e "$2 ... $GREEN SUCCESS $RESET" | tee -a $LOG_FILE
+    fi
+
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "Copying MongoDB Repo File"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling MongoDB Service"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Starting MongoDB Service"
+
+systemctl status mongod &>>$LOG_FILE
+VALIDATE $? "Checking MongoDB Service Status"
+
+#sed - stream line editor its like a vi editor command
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
+VALIDATE $? "Allowing Remote Connections in MongoDB"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB Service"
+
+systemctl status mongod &>>$LOG_FILE
+VALIDATE $? "Checking MongoDB Service Status After Restart"
