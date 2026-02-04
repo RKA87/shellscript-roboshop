5e7846f66d3502c2b9d3f63586fc24aa2efd66d6
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-04 12:51:19 -0600

    reconfig of roboshop proj

--- mongodb-v4.sh
+++ mongodb-v4.sh
@@ -20,7 +20,7 @@ LOG_FILE="$LOG_DIR/mongodb.log"
 
 #Function to print status of the executed command
 STAT_CHECK() {
-  if [ $1 --ne 0 ]; then
+  if [ $1 -ne 0 ]; then
     echo -e "${RED}FAILURE${NO}" | tee -a $LOG_FILE
     echo -e "${YELLOW}Refer the log file for more information: $LOG_FILE${NO}"
     exit 1
