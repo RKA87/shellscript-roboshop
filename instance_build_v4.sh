dc0a3eb1712a8553dd60229bf4fe60616f3a1b9e
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-04 12:20:26 -0600

    reconfig of catalogue backend

--- instance_build_v4.sh
+++ instance_build_v4.sh
@@ -1,63 +1,81 @@
 #!/bin/bash
 
-SG_ID="sg-0e3e5d0160ba94b0b" # replace with your ID
+SG_ID="sg-0e3e5d0160ba94b0b"
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z032594897Q2I0KZ7GR1"
 DOMAIN_NAME="rkak87.online"
+ROUTE53_ZONE_ID="Z032594897Q2I0KZ7GR1"
 
-for instance in $@
+#Color Codes
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+RESET="\e[0m"
+
+#check the user account has root privileges
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+  echo -e "$RED You should run this script as root user or with sudo privileges$RESET"
+  exit 1
+fi
+
+#Instance build with Route53 record creation
+for each_instance in $@
 do
+    echo -e "$YELLOW Creating EC2 Instance and Route53 record...$RESET"
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$each_instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
-
-    if [ $instance == "frontend" ]; then
-        IP=$(
+    echo -e "$GREEN Instance Created with Instance ID: $INSTANCE_ID $RESET"
+    #wait till instance up and running
+    aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+    echo -e "$GREEN $each_instance Instance ($INSTANCE_ID) is running now $RESET"
+    if  [ $each_instance == "frontend" ]; then
+        IP_ADDRESS=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
-        )
-        RECORD_NAME="$DOMAIN_NAME" # rkak87.online
+            )
+        echo -e "$GREEN Instance created with Instance ID: $INSTANCE_ID and Public IP: $IP_ADDRESS $RESET"
+        RECORD_NAME="webapp.$DOMAIN_NAME"
     else
-        IP=$(
+        IP_ADDRESS=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
-        )
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.rkak87.online
+            )
+        echo -e "$GREEN Instance created with Instance ID: $INSTANCE_ID and Private IP: $IP_ADDRESS $RESET"
+        RECORD_NAME="$each_instance.$DOMAIN_NAME"
     fi
-
-    echo "IP Address: $IP"
-
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating record",
-        "Changes": [
-            {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "'$RECORD_NAME'",
-                "Type": "A",
-                "TTL": 1,
-                "ResourceRecords": [
+    #Update Route 53 DNS Record
+    DNS_RECORD=$(
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ROUTE53_ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Updating DNS record for '$each_instance'",
+            "Changes": [
                 {
-                    "Value": "'$IP'"
+                "Action": "UPSERT",
+                "ResourceRecordSet": { 
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                    {
+                        "Value": "'$IP_ADDRESS'"
+                    }
+                    ]
                 }
-                ]
-            }
-            }
-        ]
-    }
-    '
-
-    echo "record updated for $instance"
-
+                }
+            ]
+        }
+        '
+    )
+    echo -e "$GREEN DNS record created/updated for $RECORD_NAME with IP $IP_ADDRESS $RESET"
 done
