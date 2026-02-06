6779cb3e8f50df305de0a6fd2accd15ecc43e4a8
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-06 11:33:44 -0600

    reconfig of roboshop proj

--- cart-v1.sh
+++ cart-v1.sh
@@ -1,85 +1,89 @@
 #!/bin/bash
 
-#color codes
+#Color Code
 RED="\e[31m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
-NC="\e[0m"
+NOCOLOR="\e[0m"
 
-SCRIPT_DIR=$PWD
-
-#check root user access
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-  echo -e "${RED}You should run this script as root user or with sudo privileges${NC}"
-  exit 1
-fi
-
-#Log Directory
 LOG_DIR="/var/log/shellscript-roboshop"
 mkdir -p $LOG_DIR
 
 LOG_FILE="$LOG_DIR/cart.log"
 
-#Function to print status of the executed command
-STAT_CHECK(){
-    if [ $1 -ne 0 ]; then
-      echo -e "$RED $2 ... FAILURE${NC}" | tee -a "$LOG_FILE"
-      echo -e "$YELLOW Refer the log file for more information: $LOG_FILE $NO"
-      exit 1
-    else
-      echo -e "$GREEN $2 ... SUCCESS${NC}" | tee -a "$LOG_FILE"
-    fi
+SCRIPT_DIR=$PWD
+
+#Check the user account has root privileges
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo -e "$RED You should be a root user to execute this script $NOCOLOR"
+  exit 1
+fi
+
+mkdir -p /app
+
+#Status Check Function
+STAT_CHECK() {
+  if [ $1 -ne 0 ]; then
+    echo -e "$RED $2 FAILURE $NOCOLOR" | tee -a $LOG_FILE
+    echo -e "$YELLOW Check the log file for more information $NOCOLOR"
+    exit 1
+  else
+    echo -e "$GREEN $2 ..SUCCESS.. $NOCOLOR" | tee -a $LOG_FILE
+  fi
 }
 
-dnf module disable nodejs -y | tee -a $LOG_FILE
-STAT_CHECK $? "Disable NodeJS module"
+dnf module disable nodejs -y &>>$LOG_FILE
+STAT_CHECK $? "Disable Nodejs Module"
+
+#Check Nodejs is already installed or not
 
 if dnf list installed nodejs &>>$LOG_FILE; then
-    echo -e "$YELLOW Nodejs is already installed${NC}" | tee -a $LOG_FILE
+  echo -e "$YELLOW Nodejs is already installed $NOCOLOR" | tee -a $LOG_FILE
 else
-    dnf module enable nodejs:24 -y | tee -a $LOG_FILE
-    STAT_CHECK $? "Enable NodeJS module"
-    dnf install nodejs -y | tee -a $LOG_FILE
-    STAT_CHECK $? "Installing Nodejs"
+  dnf module enable nodejs:24 -y &>>$LOG_FILE
+  dnf install nodejs -y &>>$LOG_FILE
+  STAT_CHECK $? "Installing Nodejs"
 fi
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop | tee -a $LOG_FILE
-    STAT_CHECK $? "Adding roboshop user"
+  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+  STAT_CHECK $? "Adding Roboshop User"
 else
-    echo -e "$GREEN roboshop user already exists $NC" | tee -a $LOG_FILE
+  echo -e "$YELLOW Roboshop user already exists$NOCOLOR" | tee -a $LOG_FILE
 fi
 
-#create application directory
-mkdir -p /app | tee -a $LOG_FILE
-STAT_CHECK $? "Creating application directory"
+mkdir -p /app
+STAT_CHECK $? "Creating App Directory"
 
-#download application code
+#Create App Directory and download the code
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip  &>>$LOG_FILE
 
-cd /app | tee -a $LOG_FILE
-STAT_CHECK $? "Changing directory to /app"
+cd /app
+STAT_CHECK $? "Changing Directory to /app"
 
-rm -rf /app/* | tee -a $LOG_FILE
-STAT_CHECK $? "Cleaning old application content"
+rm -rf /app/*
+STAT_CHECK $? "Remove exist app code from /app Directory"
 
-unzip /tmp/cart.zip | tee -a $LOG_FILE
-STAT_CHECK $? "Extracting cart application code"
+unzip /tmp/cart.zip &>>$LOG_FILE
+STAT_CHECK $? "Unzipping cart Code" 
 
 npm install &>>$LOG_FILE
-STAT_CHECK $? "Installing Nodejs dependencies"
+STAT_CHECK $? "Installing Nodejs Dependencies"
 
-#configure backend service
-cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
-STAT_CHECK $? "Copying cart service file"
+#Configure the backend service
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service &>>$LOG_FILE
+STAT_CHECK $? "Copying cart Service File"
 
-systemctl daemon-reload | tee -a $LOG_FILE
-STAT_CHECK $? "Reloading systemd daemon"
+systemctl daemon-reload
+STAT_CHECK $? "Daemon Reload system service"
 
-systemctl enable cart | tee -a $LOG_FILE
-STAT_CHECK $? "Enabling cart service"
+systemctl enable cart &>>$LOG_FILE
+STAT_CHECK $? "Enabling cart Service"
 
-systemctl start cart | tee -a $LOG_FILE
-STAT_CHECK $? "Starting cart service"
+systemctl start cart &>>$LOG_FILE
+STAT_CHECK $? "Starting cart Service"
+
+systemctl restart cart &>>$LOG_FILE
+STAT_CHECK $? "Restarting cart"
